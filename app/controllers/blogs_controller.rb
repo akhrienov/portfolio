51f0e648daa7760d7936c1c3ae363ec26bd6e3db
Artem Khrienov <akhrenov@livegenic.com>
2021-11-03 21:36:54 +0200

    Completed portfolio feture

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,5 +1,5 @@
 class BlogsController < ApplicationController
-  before_action :set_blog, only: %i[ show edit update destroy ]
+  before_action :set_blog, only: %i[ show edit update destroy toggle_status ]
 
   # GET /blogs or /blogs.json
   def index
@@ -49,6 +49,11 @@ class BlogsController < ApplicationController
     respond_to do |format|
       format.html { redirect_to blogs_url, notice: "Blog was successfully destroyed." }
     end
+  end
+
+  def toggle_status
+    @blog.published? ? @blog.draft! : @blog.published!
+    redirect_to blogs_url, notice: "Post status has been updated."
   end
 
   private
